dd6218ddda856f5afe97bb9596fb00888170a478
Jando-G <alexandroguterez@gmail.com>
2022-03-23 14:51:29 -0700

    Add hover effect to paint the squares black

--- javascript.js
+++ javascript.js
@@ -1,9 +1,20 @@
 for(let i = 0; i < 16; i++) {
-    document.getElementById("container").appendChild(document.createElement("div")).classList.add("flexbox");
+    document.getElementById("container")
+    .appendChild(document.createElement("div"))
+    .classList.add("flexbox");
 }
 const flexboxes = document.querySelectorAll("div.flexbox");
 flexboxes.forEach(box => {
-    for(let i = 0; i < 16; i++) {
-        box.appendChild(document.createElement("div")).classList.add("square");
+    for(let i = 0; i < flexboxes.length; i++) {
+        box.appendChild(document.createElement("div"))
+        .classList.add("square");
+    }
+})
+const squares = document.querySelectorAll("div.square");
+squares.forEach(square => {
+    for(let i = 0; i < squares.length; i++) {
+        square.addEventListener("mouseenter", (e) => {
+            e.target.style.backgroundColor = 'black';
+        })
     }
 })
